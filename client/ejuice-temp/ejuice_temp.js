5b219b89957a25b68f514284ccc066928a414ea4
phtn458 <phtn458@gmail.com>
2015-12-01 10:53:03 -0500

    ejuice temp styl change

--- client/ejuice-temp/ejuice_temp.js
+++ client/ejuice-temp/ejuice_temp.js
@@ -1,3 +1,7 @@
+Template.ejuiceTemp.rendered = ()=> {
+	Session.seDefault('ejuice-price', 13.99)
+}
+
 Template.ejuiceTemp.helpers({
 	name () {
 		return Session.get('ejuice-name')
@@ -7,5 +11,9 @@ Template.ejuiceTemp.helpers({
 	},
 	url () {
 		return Session.get('ejuice-url')
+	},
+	price () {
+		return Session.get('ejuice-price')
 	}
-})
+});
+
